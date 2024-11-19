a8f09a5055a6f44c112b7109121e419988fc89be
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-11-19 09:52:22 +0900

    fix global menu, self publish

--- wp-content/themes/hokutoshobo/header.php
+++ wp-content/themes/hokutoshobo/header.php
@@ -49,14 +49,28 @@
     </header>
 
     <ul class="global-menu">
-      <li><a href="<?php echo esc_url(home_url('/')); ?>">ホーム</a></li>
-      <li><a href="<?php echo esc_url(home_url('/feature')); ?>">当社の特徴</a></li>
-      <li><a href="<?php echo esc_url(home_url('/price')); ?>">料金プラン</a></li>
-      <li><a href="<?php echo esc_url(home_url('/process')); ?>">自費出版<br />制作の流れ</a></li>
-      <li><a href="<?php echo esc_url(home_url('/knowledge')); ?>">豆知識</a></li>
-      <li><a href="<?php echo esc_url(home_url('/voice')); ?>">お客様の声</a></li>
-      <li><a href="<?php echo esc_url(home_url('/faq')); ?>">Q&nbsp;&&nbsp;A</a></li>
-      <li><a href="<?php echo esc_url(home_url('/access')); ?>">アクセス</a></li>
+      <li class="global-menu__link"><a href="<?php echo esc_url(home_url('/')); ?>">ホーム</a></li>
+      <li class="global-menu__link"><a href="<?php echo esc_url(home_url('/feature')); ?>">当社の特徴</a></li>
+      <li class="global-menu__link"><a href="<?php echo esc_url(home_url('/price')); ?>">料金プラン</a></li>
+      <li class="global-menu__link"><a href="<?php echo esc_url(home_url('/process')); ?>">自費出版<br />制作の流れ</a></li>
+      <li class="global-menu__link">
+        <a href="#">自費出版<br />アラカルト</a>
+        <div class="global-menu__sub-link knowledge">
+          <a href="<?php echo esc_url(home_url('/knowledge')); ?>">自費出版の<br />豆知識</a>
+        </div>
+        <div class="global-menu__sub-link novel-self-publish">
+          <a href="<?php echo esc_url(home_url('/novel-self-publish')); ?>">文字もの<br />自費出版</a>
+        </div>
+        <div class="global-menu__sub-link visual-self-publish">
+          <a href="<?php echo esc_url(home_url('/visual-self-publish')); ?>">ビジュアル系<br />自費出版</a>
+        </div>
+        <div class="global-menu__sub-link welcome-msword">
+          <a href="<?php echo esc_url(home_url('/welcome-msword')); ?>">データ入稿<br />対応可能</a>
+        </div>
+      </li>
+      <li class="global-menu__link"><a href="<?php echo esc_url(home_url('/voice')); ?>">お客様の声</a></li>
+      <li class="global-menu__link"><a href="<?php echo esc_url(home_url('/faq')); ?>">Q&nbsp;&&nbsp;A</a></li>
+      <li class="global-menu__link"><a href="<?php echo esc_url(home_url('/access')); ?>">アクセス</a></li>
     </ul>
 
     <div class="mobile-menu__btn">
